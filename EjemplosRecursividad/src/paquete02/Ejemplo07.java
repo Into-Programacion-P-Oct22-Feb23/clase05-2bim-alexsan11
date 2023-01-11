20439dcaf154dcf3da1747ad0e4b629093cf79e4
Christopher Jaramillo <fjarmijos1@utpl.edu.ec>
2023-01-11 09:18:37 -0500

    clase semana 5

--- EjemplosRecursividad/src/paquete02/Ejemplo07.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo07.java
@@ -18,6 +18,20 @@ public class Ejemplo07 {
         // 2 elevado a la potencia 2 (cuadrado)
         // 2 elevado a la potencia 3 (cubo)
         
+        
+        for (int i = 0; i < valores.length; i++) {
+            for (int j = 0; j < potencias.length; j++) {
+                
+                System.out.printf("%d elevado a la potencia %d %d\n",
+                        valores[i],
+                        potencias[j],
+                        obtenerPotencia (valores[i],potencias[j] ));
+                
+                
+            }
+            
+        }
+        
     }
 
     public static int obtenerPotencia(int base, int exponente) {
